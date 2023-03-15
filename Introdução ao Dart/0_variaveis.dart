c1a6f0d635e099cc60b93398394dd75b9e0acee2
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-03-14 21:55:34 -0300

    Primeiras aulas de Dart

---
+++ Introdução ao Dart/0_variaveis.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Olá Uninove"); //você pode dar print com " ou ' (mantenha um padrão_)
+  
+  String nome = "Thiago Traue"; //representa uma variável textual
+  int idade = 35;
+  double altura = 1.83;
+  bool fumante = false;
+  
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Altura: $altura");
+  print("Fuma? $fumante");
+}
