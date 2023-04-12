05116091526ef1171ab39d53306c7ff002a186cc
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-04-12 01:53:19 +0100

    Continuação

--- etanol_gasolina/lib/main.dart
+++ etanol_gasolina/lib/main.dart
@@ -15,6 +15,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController etanolController =  TextEditingController();
+  TextEditingController gasolinaController = TextEditingController();
+  String _resultado = "Informe os valores";
+
+  void _calculaCombustivelIdeal() {
+    double vEtanol = double.parse(etanolController.text.replaceAll(",", "."));
+    double vGasolina = double.parse(gasolinaController.text.replaceAll(",", "."));
+    double proporcao = vEtanol / vGasolina;
+
+    setState(() {
+      _resultado = (proporcao < 0.7) ? "Abasteça com Etanol" : "Abasteça com Gasolina";
+    });    
+
+    // ^ é o mesmo que...:
+    // if (proporcao < 0.7) {
+    //   _resultado = "Abasteça com Etanol";
+    // } else {
+    //   _resultado = "Abasteça com Gasolina";
+    // }
+  }
+
+  void _reset() {
+    etanolController.text = "";
+    gasolinaController.text = "";
+    setState(() {
+      _resultado = "Informe os valores";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +55,7 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
               onPressed: () {
-                //depois voltamos aqui
+                _reset();
               },
               icon: const Icon(Icons.refresh))
         ],
@@ -42,6 +72,7 @@ class _HomeState extends State<Home> {
                 color: Colors.lightBlue[900],
               ),
               TextFormField(
+                controller: etanolController,
                 textAlign: TextAlign.center,
                 keyboardType: const TextInputType.numberWithOptions(decimal: true),
                 decoration: InputDecoration(
@@ -51,9 +82,10 @@ class _HomeState extends State<Home> {
                 style: TextStyle(
                   fontSize: 26,
                   color: Colors.lightBlue[900]
-                ),
+                )
               ),
               TextFormField(
+                controller: gasolinaController,
                 textAlign: TextAlign.center,
                 keyboardType: const TextInputType.numberWithOptions(decimal: true),
                 decoration: InputDecoration(
@@ -63,13 +95,36 @@ class _HomeState extends State<Home> {
                 style: TextStyle(
                   fontSize: 26,
                   color: Colors.lightBlue[900]
-                ),
+                )
               ),
-              //paramos aqui
-            ],
-          ),
-        ),
-      ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, bottom: 26),
+                child: SizedBox(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.lightBlue[900]
+                    ),
+                    child: const Text(
+                      "Verificar",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                    onPressed: () {
+                      _calculaCombustivelIdeal();
+                    },
+                  )
+                )
+              ),
+              Text(
+                _resultado,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.lightBlue[900], fontSize: 26),
+              )
+            ]
+          )
+        )
+      )
     );
   }
 }
