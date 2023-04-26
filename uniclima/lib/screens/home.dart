ddcc3b58f32f251278df3d28a708c420e3d4a66a
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-04-26 01:58:16 +0100

    Continuação

--- uniclima/lib/screens/home.dart
+++ uniclima/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +9,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final List<String> _cidades = [
+    'Aracaju',
+    'Belém',
+    'Belo Horizonte',
+    'Boa Vista',
+    'Brasilia',
+    'Campo Grande',
+    'Cuiabá',
+    'Curitiba',
+    'Florianópolis',
+    'Fortaleza',
+    'Goiânia',
+    'João Pessoa',
+    'Macapá',
+    'Maceió',
+    'Manaus',
+    'Natal',
+    'Palmas',
+    'Porto Alegre',
+    'Porto Velho',
+    'Recife',
+    'Rio Branco',
+    'Rio de Janeiro',
+    'Salvador',
+    'São Luís',
+    'São Paulo',
+    'Teresina',
+    'Vitória'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UniClima'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            DropdownSearch<String>(
+              items: _cidades,
+              selectedItem: 'São Paulo',
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
