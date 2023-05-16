68bf36161a3147aacdd1cdda76389205f6f25726
Prof. Thiago G. Traue <t.traue@gmail.com>
2023-05-17 00:55:36 +0100

    Update api_constants.dart

--- uniclima_v2/lib/constants/api_constants.dart
+++ uniclima_v2/lib/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   static const apiUrl = 'api.openweathermap.org';
-  static const apiKey = '8fa1810f70b5a33ef587be61b1b8256c'; //cada um deve ter a SUA
+  static const apiKey = 'XXXX'; //cada um deve ter a SUA
   static const geoPath = '/geo/1.0/direct';
   static const weatherPath = '/data/2.5/weather';
   static const units = 'metric';
